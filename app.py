9405a37039af1e3e32fa4c8ff8e97bc95cb47d00
annga <75178572+Anna-grf@users.noreply.github.com>
2023-11-20 20:32:46 +0100

    store queries in seperate py files

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from neo4j import GraphDatabase
 from py2neo import Graph
 
+from queries.coolant_query import get_coolant_data
+from queries.handling_device_query import get_handling_device_data
+from queries.injection_molding_machine_query import run_injection_molding_machine_query
+
 app = Flask(__name__)
 
 load_dotenv()
@@ -13,54 +17,6 @@ neo4j_password = "engx1494"
 
 graph = Graph(neo4j_uri, auth=(neo4j_username, neo4j_password))
 
-def get_handling_device_data(uri, username, password):
-    with GraphDatabase.driver(uri, auth=(username, password)) as driver:
-        with driver.session() as session:
-            query = """
-            MATCH (i:Inquiry_1)
-            WITH i.HandlingDevice AS InquiryHandlingDevice
-            MATCH (tcu)
-            WHERE tcu.ManufacturerProductRoot = 'HandlingDevice'
-            AND tcu.HandlingDevice = InquiryHandlingDevice
-            RETURN InquiryHandlingDevice, COLLECT(tcu) AS MatchingControlUnits
-            """
-            result = session.run(query)
-            return result.data()
-        
-def run_injection_molding_machine_query(graph):
-    cypher_query = """
-    MATCH (inquiry:Inquiry_1)
-    MATCH (imm)
-    WHERE imm.ManufacturerProductRoot = 'InjectionMoldingMachine'
-    WITH inquiry, imm,
-      (inquiry.MinRequiredClampingForce <= imm.MaxClampingForce) AS OK1,
-      (inquiry.ShotVolume <= imm.MaxPlasticizingCapacity) AS OK2,
-      (inquiry.GreaterMoldDimension <= imm.GreaterClearDistanceBetweenColumns) AS OK3,
-      (inquiry.SmallerMoldDimension <= imm.SmallerClearDistanceBetweenColumns) AS OK4,
-      (inquiry.RequiredOpeningStroke <= imm.MaxOpeningStroke) AS OK5
-    RETURN imm.idShort AS idShort,
-      CASE
-        WHEN OK1 AND OK2 AND OK3 AND OK4 AND OK5 THEN 'is technically feasible'
-        ELSE 'is not technically feasible'
-      END AS feasibility
-    """
-
-    result = graph.run(cypher_query).data()
-    return result
-
-def get_coolant_data(uri, username, password):
-    with GraphDatabase.driver(uri, auth=(username, password)) as driver:
-        with driver.session() as session:
-            query = """
-            MATCH (i:Inquiry_1)
-            WITH i.Coolant AS InquiryCoolant
-            MATCH (tcu)
-            WHERE tcu.ManufacturerProductRoot = 'TemperatureControlUnit'
-            AND tcu.Coolant = InquiryCoolant
-            RETURN InquiryCoolant, COLLECT(tcu) AS MatchingControlUnits
-            """
-            result = session.run(query)
-            return result.data()
 
 @app.route('/')
 def index():
--- queries/coolant_query.py
+++ queries/coolant_query.py
@@ -13,18 +13,3 @@ def get_coolant_data(uri, username, password):
             """
             result = session.run(query)
             return result.data()
-
-neo4j_uri = "bolt://localhost:7687"
-neo4j_username = "neo4j"
-neo4j_password = "engx1494"
-
-coolant_data = get_coolant_data(neo4j_uri, neo4j_username, neo4j_password)
-
-for record in coolant_data:
-    print("InquiryCoolant:", record["InquiryCoolant"])
-    print("Coolant Comparison:")
-    matching_control_units = record["MatchingControlUnits"]
-    if matching_control_units:
-        for unit in matching_control_units:
-            print("Matched with:", unit["idShort"])  
-    print()
